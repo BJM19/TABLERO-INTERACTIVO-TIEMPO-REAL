8c32e923bbe34f1f9d777f33188185abdec65a58
BJM19 <brayan.jimenez@mail.escuelaing.edu.co>
2021-06-30 22:00:49 -0500

    Load Dates

---
+++ src/main/java/co/escuelaing/edu/interactiverealtime/BBAppStarter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.escuelaing.edu.interactiverealtime;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class BBAppStarter {
+ public static void main(String[] args){
+ SpringApplication.run(BBAppStarter.class,args);
+ }
+}
